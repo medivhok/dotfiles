d589bdbb3a13713efcd1c2fc72973ac5686c190e
Jean Gregory Verret <gregory.verret@intelia.com>
2019-02-12 13:17:24 -0500

    update

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,21 +1,33 @@
 import XMonad
+import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.SpawnOnce
 import System.IO
 
+
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar"
-  xmonad $ defaultConfig
-    { terminal   = "termite"
-    , modMask    = mod4Mask
-    , manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    , logHook = dynamicLogWithPP xmobarPP
-                { ppOutput = hPutStrLn xmproc
-                , ppTitle = xmobarColor "green" "" . shorten 50
-                }
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ def
+    { terminal        = "termite"
+    , modMask         = mod4Mask
+    , workspaces      = ["1:Code", "2:Shell", "3:Web", "4:Files", "5:Graphics", "6:FTP", "7:Office", "8:Profit", "9:Misc."]
+    , manageHook      = manageDocks <+> manageSpawn <+> manageHook def
+    , layoutHook      = avoidStruts  $  layoutHook def
+    , handleEventHook = def <+> docksEventHook
+    , logHook         = dynamicLogWithPP xmobarPP
+        { ppOutput          = hPutStrLn xmproc
+        , ppTitle           = xmobarColor "darkgreen" "" . shorten 90
+        , ppHiddenNoWindows = xmobarColor "grey" ""
+        }
+    , startupHook = do
+        spawnOnce "compton"
+        spawnOnce "xcape -t 300 -e \"Control_L=Escape;Control_R=Return\""
+        spawnOnce "light-locker"
+        spawnOn "3" "chromium"
+        spawnOnce "stalonetray"
     } `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_z), spawn "light-locker-command --lock")
     , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
