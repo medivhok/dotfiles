351d1eaa266f21d720362a5ae2e882c012015427
Jean Gregory Verret <gregory.verret@gmail.com>
2020-04-06 18:06:08 -0400

    emacs config update

--- src/.config/doom/config.el
+++ src/.config/doom/config.el
@@ -45,10 +45,23 @@
   :custom
   (dashboard-startup-banner 'logo)
   (dashboard-show-shortcuts nil)
+  (dashboard-set-navigator t)
+  (dashboard-set-heading-icons t)
+  (dashboard-set-file-icons t)
   (dashboard-items '((recents . 5)
 		     (projects . 3)
 		     (agenda . 5)))
   :config
+  ;; (defun medivhok/dashboard-insert-custom (list-size)
+  ;;   "Add the list of LIST-SIZE items of menu."
+  ;;   (dashboard-insert-section
+  ;;    "Menu:"
+  ;;    '("Budget" "Org")
+  ;;    2
+  ;;    "l"
+  ;;    `(lambda (&rest ignore))))
+  ;; (add-to-list 'dashboard-item-generators '(custom . medivhok/dashboard-insert-custom))
+  ;; (add-to-list 'dashboard-items '(custom) t)
   (dashboard-setup-startup-hook))
 
 ;; Display lambda as symbols
@@ -447,10 +460,6 @@
                                              ((org-agenda-overriding-header "One-off Tasks")
                                               (org-agenda-files '(,(concat jethro/org-agenda-directory "next.org")))
                                               (org-agenda-skip-function '(org-agenda-skip-entry-if 'deadline 'scheduled)))))))))
-
-(add-hook 'org-mode-hook #'writeroom-mode)
-;;(add-hook 'writeroom-mode-hook #'+word-wrap-mode)
-(add-hook 'writeroom-mode-hook #'+org-pretty-mode)
 
 (use-package! org-roam
   :commands (org-roam-insert org-roam-find-file org-roam-switch-to-buffer org-roam)
